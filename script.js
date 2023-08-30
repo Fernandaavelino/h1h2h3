bf58500d430603cd5e3d0817483986a87f16e3a4
Fernanda Novais Avelino <126677085+Fernandaavelino@users.noreply.github.com>
2023-08-30 16:04:05 -0300

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("vermelho").style.color= "red";
+document.getElementById("italico").style.fontStyle= "italic";
+document.getElementById("centralizado").style.textAlign= "center";
+document.getElementById("borda").style.border= "solid 1px bleue";
